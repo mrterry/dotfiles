0315cb6caebda1a0057aeffeb67f36dad4064423
Matt Terry <mrterry@modular.com>
2023-05-26 09:03:15 -0700

    improve startup

--- install.sh
+++ install.sh
@@ -2,8 +2,18 @@
 
 set -e
 
+# finish configuring the repo
+cd "$HOME"/src/dotfiles
+git submodule init
+git submodule update
+
+# move to home so we can configure dotfiles
 cd "$HOME"
 
+# configure vim
 ln -s "$HOME"/src/dotfiles/vim .vim
 ln -s "$HOME"/src/dotfiles/vim/vimrc .vimrc
+
+# configure git
+cat "$HOME"/src/dotfiles/git/gitconfig .gitconfig
 
